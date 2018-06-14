0ec2425525f1c7e86c21114f12cc2c639abc8eaf
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2018-06-15 00:36:55 +0200

    Moved ics file. More info on index.php

--- publish/index.php
+++ publish/index.php
@@ -5,6 +5,15 @@
   </head>
   <body>
     <h1>Kalender med VM-kampene 2018</h1>
-    <a href="webcal://dogp.no/vm2018/calendars/fifa_vm_2018_nb.ics" >Kalender</a>
+    <h3>
+      <a href="webcal://dogp.no/vm2018/calendars/vm2018.ics">Kalender</a>
+    </h3>
+    <h3>
+      <a href="https://calendar.google.com/calendar/embed?src=icaqausnab0ckfip41j7o6ne3rj0g32b%40import.calendar.google.com&ctz=Europe%2FOslo" >Google kalender</a>
+    </h3>
+    <h2>
+      Nedenfor er selve kalenderen:
+    </h2>
+    <iframe src="https://calendar.google.com/calendar/embed?src=icaqausnab0ckfip41j7o6ne3rj0g32b%40import.calendar.google.com&ctz=Europe%2FOslo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
   </body>
 </html>
